b05457be4f3b3ee8e2c211a55fcf95e411eb8a7a
miki623 <68592086+miki623@users.noreply.github.com>
2020-09-12 02:36:31 +0900

    コントローラーの追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    if resource_class == Seller
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, :jender, :info, :schedule])
+    elsif resource_class == Buyer
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, :jender])
+    else
+      super
+    end
+  end
 end
