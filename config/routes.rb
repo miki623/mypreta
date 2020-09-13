83659d00badf6fa7f5f26f58046613de8323adcb
miki623 <68592086+miki623@users.noreply.github.com>
2020-09-13 22:56:33 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :buyers
   devise_for :sellers
-  resources :items
+  resources :items do
+    member do
+      get 'confirmation'
+    end
+  end
   resources :categories, only:[:show]
   resources :sellers, only:[:show]
   
