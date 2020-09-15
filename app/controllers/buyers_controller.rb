ec6b71677e0109a2175b83a8b18ebcf7531f391c
miki623 <68592086+miki623@users.noreply.github.com>
2020-09-15 22:09:31 +0900

    コントローラーの修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,9 @@
 class BuyersController < ApplicationController
+
+  def show
+    # binding.pry
+    @buyer = Buyer.find(params[:id])
+    # deals = @buyer.deal_ids
+    @deals = @buyer.deals.all
+  end
 end
