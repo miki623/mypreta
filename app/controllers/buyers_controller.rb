b88f71b476448089b8dc7518b2859592a1833e8f
miki623 <68592086+miki623@users.noreply.github.com>
2020-09-18 13:02:57 +0900

    コントローラーの修正・追記

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,7 +1,6 @@
 class BuyersController < ApplicationController
 
   def show
-    # binding.pry
     @buyer = Buyer.find(params[:id])
     # deals = @buyer.deal_ids
     @deals = @buyer.deals.all
--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -2,6 +2,11 @@ class SellersController < ApplicationController
   def show
     @seller = Seller.find(params[:id])
     @items = @seller.items
-    
+  end
+
+  def history
+    @seller = Seller.find(params[:id])
+    @deals = @seller.deals.all
+
   end
 end
