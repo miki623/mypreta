b85b9eb010b5aa2fd21266a30a8a713044ab627a
miki623 <68592086+miki623@users.noreply.github.com>
2020-09-27 23:19:42 +0900

    修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def show
-    @items = Item.where(category_id: params[:id])
+    @items = Item.where(category_id: params[:id]).includes(:seller)
     @category = Category.find(params[:id])
   end
 
