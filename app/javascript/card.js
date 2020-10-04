9ae82d1fbe815ba3a7fa47aa24bc9ae95a0a82ea
miki623 <68592086+miki623@users.noreply.github.com>
2020-10-04 17:47:37 +0900

    commit

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,6 @@
 
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  console.log("PAYJP_PUBLIC_KEY:  " + process.env.PAYJP_PUBLIC_KEY)
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
